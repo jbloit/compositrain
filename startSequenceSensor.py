24d3093e220b7acfe9b1b4aa12f4593f1386886d
Julien Bloit <julien.bloit@gmail.com>
2019-04-14 17:41:19 +0200

    rpi: read a GPIO pin and send OSC message

---
+++ startSequenceSensor.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+INPUT_PIN = 10
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+client = udp_client.SimpleUDPClient("192.168.1.16", 12345)
+
+
+
+def buttonEvent(channel):
+    print("ACTION ON PIN " + str(channel) )
+    client.send_message("/start", channel)
+
+GPIO.add_event_detect(INPUT_PIN, GPIO.BOTH, callback=buttonEvent)
+
+    
+"""
+while(True):
+    if GPIO.input(INPUT_PIN) == GPIO.HIGH:
+        print("PUSHED")
+"""
+
+message = input("press any key to quit")
+
